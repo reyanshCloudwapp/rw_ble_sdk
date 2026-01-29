6c388bd300193b1bed961f2db7a52fb7de831202
reyanshCloudwapp <reyansh.cloudwapp@gmail.com>
2026-01-29 17:15:12 +0530

    ai changes

--- example/android/settings.gradle.kts
+++ example/android/settings.gradle.kts
@@ -14,26 +14,15 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
-        flatDir {
-            dirs("libs")
-        }
     }
 }
 
-//plugins {
-//    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-//    id("com.android.application") version "8.11.1" apply false
-//    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
-//}
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.11.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+}
 
 rootProject.name = "example"
 
-// Include the app module
 include(":app")
-//project(":app").projectDir = file("app")
-
-// Include the library module
-include(":rw_ble")
-project(":rw_ble").projectDir = file("../rw_ble")
-
-
